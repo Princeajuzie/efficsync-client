d3ed48d42ab0d7675779e9db0be096f66184350f
princeajuzie7 <princeajuzie03@gmail.com>
2024-03-06 13:20:09 +0100

    feat: new config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
-
+import Link from "next/link";
 export default function Home() {
   return (
    <main>
     <h1>hello world</h1>
+    <Link href={`/auth/signup`}>
+
+    <div>auth</div>
+    </Link>
    </main>
   );
 }
