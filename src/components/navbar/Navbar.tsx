30e1286fd480ef8ba124e6754f3e98d922672652
princeajuzie7 <princeajuzie03@gmail.com>
2024-03-06 17:18:48 +0100

    feat: new changes

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -19,37 +19,7 @@ export function Navbar() {
               className="h-auto w-[40px]  "
             />
           </div>
-          <div className=" ">
-            <label htmlFor="icon" className="sr-only">
-              Search
-            </label>
-            <div className="relative">
-              <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-4">
-                <svg
-                  className="flex-shrink-0 size-4 text-gray-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={24}
-                  height={24}
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <circle cx={11} cy={11} r={8} />
-                  <path d="m21 21-4.3-4.3" />
-                </svg>
-              </div>
-              <input
-                type="text"
-                id="icon"
-                name="icon"
-                className="py-2 px-4 ps-11 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
-                placeholder="Search"
-              />
-            </div>
-          </div>
+      
           <div className="flex flex-row items-center justify-end gap-2">
             <button
               type="button"
