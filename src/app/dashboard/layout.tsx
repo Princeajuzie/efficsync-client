ac27eeefef8af12936331f167045c4f568eafd3f
princeajuzie7 <princeajuzie03@gmail.com>
2024-03-09 12:08:18 +0100

    feat: new update

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,12 +3,12 @@ import { Navbar, Sidebar, BottomNav } from "@/components";
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="relative">
-      <main className="flex flex-col lg:flex-row w-screen overflow-hidden h-screen">
+      <main className="flex flex-col lg:flex-row w-full  h-full">
         <Sidebar />
 
         <div className="">
           <Navbar />
-          <div className=" bg-red-600 w-[100%] flex flex-col  lg:overflow-y-auto lg:h-[calc(100vh-50px)] h-[calc(100svh-135px)] overflow-y-auto ">
+          <div className=" bg-red-600 w-[100%] flex flex-col  lg:overflow-y-auto lg:h-[auto] h-[calc(100svh-135px)] overflow-y-auto ">
             {children}
           </div>
           <div className="z-20">
