7324824e61262b58ac7d4ee43a24d27bea0a0f46
princeajuzie7 <princeajuzie03@gmail.com>
2024-03-23 15:27:35 +0100

    feat: new config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,8 @@
-import Image from "next/image";
-import Link from "next/link";
-import Kabantest from "@/components/test/Kabantest";
-import Kabantest1 from "@/components/test/Kabantest copy";
-import { Navbar, Sidebar, BottomNav } from "@/components/dashboard";
+
 export default function Home() {
   return (
     <main className="">
-      {/* <Kabantest1 /> */}
-
-      <div className=" ">{/* <Navbar /> */}</div>
-
-      {/* <div className="">
-        <Sidebar />
-      </div> */}
+  
     </main>
   );
 }
