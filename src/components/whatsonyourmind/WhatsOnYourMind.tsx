27be193b4e0850beabe5fa769b515e77a2a6e6fa
princeajuzie7 <princeajuzie03@gmail.com>
2024-03-23 06:16:02 +0100

    config

--- src/components/whatsonyourmind/WhatsOnYourMind.tsx
+++ src/components/whatsonyourmind/WhatsOnYourMind.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export function WhatsOnYourMind() {
   return (
-    <div className="w-full z-10 bg-[#f8f7fd] py-3 px-2">
+    <div className="w-full z-10  py-3 px-2">
       <button
-        data-e2e="fresh-widget-button"
-        className="  mx-auto flex  cursor-pointer overflow-auto rounded-2xl bg-white p-4 text-left text-kimberly drop-shadow hover:drop-shadow-md  w-full"
+        data-e2e=""
+        className="  mx-auto flex  cursor-pointer overflow-auto rounded-2xl bg-white p-4 text-left text-kimberly shadow-md hover:drop-shadow-md  w-full"
         class-list="bg-mist hover:bg-dust"
       >
         <span className="mr-2 flex h-10 w-10 flex-shrink-0 overflow-hidden rounded-full">
