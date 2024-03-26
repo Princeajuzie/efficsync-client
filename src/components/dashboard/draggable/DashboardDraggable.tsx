aff265165b9a7eefa267dff16363c914963abc7c
princeajuzie7 <princeajuzie1@gmail.com>
2024-03-26 14:53:09 +0100

    feat: new config

--- src/components/dashboard/draggable/DashboardDraggable.tsx
+++ src/components/dashboard/draggable/DashboardDraggable.tsx
@@ -210,7 +210,7 @@ export function DashboardDraggable() {
             viewBox="0 0 16 16"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
-            className="w-4"
+            className="w-4 dragging"
           >
             <path
               fillRule="evenodd"
